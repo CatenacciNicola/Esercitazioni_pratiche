89a1074d08ebe9433313f52a0e2558223abf933e
Catenacci Nicola <catenaccinicola@outlook.it>
2024-06-21 15:11:41 +0200

    Commenti

---
+++ Esercitazion_pratica_13/Esercitazione_11/Esercitazione_11/Contribuente.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione_11
+{
+    //ENUMERAZIONE PER RAPPRESENTARE IL SESSO
+    public enum Sesso
+    {
+        M,
+        F
+    }
+    internal class Contribuente
+    {
+        //PROPRIETA' DEL CONTRIBUENTE
+        public string Nome { get; private set; }
+        public string Cognome { get; private set; }
+        public DateOnly DataNascita { get; private set; }
+        public string CodiceFiscale { get; private set; }
+        public Sesso Sesso { get; private set; }
+        public string ComuneResidenza { get; private set; }
+        public decimal RedditoAnnuale { get; private set; }
+
+        //COSTRUTTORE 
+        public Contribuente(string nome, string cognome, DateOnly dataNascita, string codiceFiscale, Sesso sesso, string comuneResidenza, decimal redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        //METODO PER CALCOLARE L'IMPOSTA DATO IL REDDITO. VIENE ESEGUITO L'IF/ELSE IF IN BASE ALLA CIFRA INSERITA
+        public decimal CalcolaImposta()
+        {
+            decimal imposta = 0;
+            decimal reddito = RedditoAnnuale;
+
+            if (reddito <= 15000)
+            {
+                imposta = reddito * 23 / 100;
+            }
+            else if (reddito <= 28000)
+            {
+                imposta = 3450 + ((reddito - 15000) * 27 / 100);
+            }
+            else if (reddito <= 55000)
+            {
+                imposta = 6960 + ((reddito - 28000) * 38 / 100);
+            }
+            else if (reddito <= 75000)
+            {
+                imposta = 17220 + ((reddito - 55000) * 41 / 100);
+            }
+            else
+            {
+                imposta = 25420 + ((reddito - 75000) * 43 / 100);
+            }
+
+            return imposta;
+        }
+
+        //METODO PER STAMPARE A VIDEO I DATI DEL CONTRIBUENTE E L'IMPOSTA CALCOLATA
+        public void StampaDettagli()
+        {
+            Console.WriteLine($"Contribuente: {Nome} {Cognome}");
+            Console.WriteLine($"Nato il: {DataNascita.ToString("dd/MM/yyyy")} ({Sesso})");
+            Console.WriteLine($"Residente in: {ComuneResidenza}");
+            Console.WriteLine($"Codice fiscale: {CodiceFiscale}");
+            //CONVERTO LE CIFRE NEL FORMATO SCELTO ("C", new CultureInfo("it-IT"))
+            Console.WriteLine($"Reddito dichiarato: {RedditoAnnuale.ToString("C", new CultureInfo("it-IT"))}");
+            Console.WriteLine($"IMPOSTA DA VERSARE: {CalcolaImposta().ToString("C", new CultureInfo("it-IT"))}");
+        }
+    }
+}
---
+++ Esercitazion_pratica_13/Esercitazione_11/Esercitazione_11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace Esercitazione_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CHIEDO ALL'UTENTE DI INSERIRE I DATI
+            Console.Write("Inserisci il nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Inserisci il cognome: ");
+            string cognome = Console.ReadLine();
+
+            //LA DATA VA NECESSARIAMENTE IN FORMATO gg/mm/aaaa
+            Console.Write("Inserisci la data di nascita (gg/mm/aaaa): ");
+            DateTime dataNascitaInput = DateTime.Parse(Console.ReadLine());
+            DateOnly dataNascita = DateOnly.FromDateTime(dataNascitaInput);
+
+            Console.Write("Inserisci il codice fiscale: ");
+            string codiceFiscale = Console.ReadLine();
+
+            //IL CICLO VIENE ESEGUITO FINO A CHE NON VIENE INSERITO UN PARAMETRO ADATTO.
+            //ENUM.TRYPARSE TENTA LA CENVERSIONE DEL DATO INSERITO IN UNO DELL'ENUM
+            //TRUE TOGLIE IL CASE-SENSITIVE RENDENDO m=M e f=F
+            Sesso sesso;
+            do
+            {
+                Console.Write("Inserisci il sesso (M/F): ");
+            } 
+            while (!Enum.TryParse(Console.ReadLine(), true, out sesso));
+
+            Console.Write("Inserisci il comune di residenza: ");
+            string comuneResidenza = Console.ReadLine();
+
+            Console.Write("Inserisci il reddito annuale: ");
+            decimal redditoAnnuale = decimal.Parse(Console.ReadLine());
+
+            //CREO UNA NUOVA ISTANZA DI CONTRIBUENTE CON I DATI INSETIRI DALL'UTENTE
+            Contribuente contribuente = new Contribuente(nome, cognome, dataNascita, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+            //SIMULO TEMPO DI ATTESA
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE:");
+            
+            //RICHIAMO IL METODO CHE STAMPA TUTTI I DETTAGLI E L'IMPOSTA CALCOLATA
+            contribuente.StampaDettagli();
+        }
+    }
+}
