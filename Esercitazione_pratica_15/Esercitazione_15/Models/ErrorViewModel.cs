d866ada2f2b6738727e01903de711ce3cb9d13f7
Catenacci Nicola <catenaccinicola@outlook.it>
2024-07-05 15:06:35 +0200

    Nella cartella SQL si trovano i TXT

---
+++ Esercitazione_pratica_15/Esercitazione_15/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esercitazione_15.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
